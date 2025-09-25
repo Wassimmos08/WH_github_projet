d63011688c45dc82b3b7417026e9484f7ef39d7f
Wassimmos08 <mowassim17@gmail.com>
2025-09-25 12:14:39 +0100

    Merge local changes with remote

--- react/src/models/user.jsx
+++
@@ -1,7 +0,0 @@
-// Fixed user.js
-export default class User {
-    constructor(email, password) {  // Fixed parameter names
-        this.email = email;        // Fixed: was assigning to undefined 'username'
-        this.password = password;
-    }
-}
